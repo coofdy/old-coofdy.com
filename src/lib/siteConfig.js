0e41710ed3c3b349bc6c2e7b0c48c816bc4de42e
Martin Kenny <git@mkenny.net>
2023-08-15 12:02:22 +0930

    Initial commit.

---
+++ src/lib/siteConfig.js
@@ -0,0 +1,19 @@
+export const SITE_URL = 'https://mkennycom.netlify.app';
+export const APPROVED_POSTERS_GH_USERNAME = ['coofdy'];
+export const GH_USER_REPO = 'coofdy/mkenny.com'; // used for pulling github issues and offering comments
+export const SITE_TITLE = 'MKenny.com';
+export const SITE_DESCRIPTION = "Martin Kenny's Blog";
+export const DEFAULT_OG_IMAGE =
+  'https://user-images.githubusercontent.com/6764957/147861359-3ad9438f-41d1-47c8-aa05-95c7d18497f0.png';
+export const MY_TWITTER_HANDLE = 'coofdy';
+// export const MY_YOUTUBE = 'https://youtube.com/coofdy';
+export const POST_CATEGORIES = ['Essay', 'Note']; // Other categories you can consider adding: Talks, Tutorials, Snippets, Podcasts, Notes...
+export const GH_PUBLISHED_TAGS = ['Published'];
+
+// auto generated variables
+export const REPO_URL = 'https://github.com/' + GH_USER_REPO;
+export const REPO_OWNER = GH_USER_REPO.split('/')[0];
+
+// dont forget process.env.GH_TOKEN
+// if supplied, raises rate limit from 60 to 5000
+// https://docs.github.com/en/rest/overview/resources-in-the-rest-api#rate-limiting
